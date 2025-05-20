029da51f58f78e48e73db6696706d1f4b10617d7
RichardArce <richard.arce.vargas@gmail.com>
2025-05-19 19:35:51 -0600

    Validación personalizada

--- CursoProgramacionAvanzadaWeb/Models/CarroViewModel.cs
+++ CursoProgramacionAvanzadaWeb/Models/CarroViewModel.cs
@@ -1,9 +1,14 @@
+using CursoProgramacionAvanzadaWeb.Validators;
+using System.ComponentModel.DataAnnotations;
+
 namespace CursoProgramacionAvanzadaWeb.Models
 {
     public class CarroViewModel
     {
+        [Required(ErrorMessage = "La marca del carro es  requerida")]
         public string? marca { get; set; }
 
+        [CarroModeloValidator]
         public string? modelo { get; set; }
     }
 }
---
+++ CursoProgramacionAvanzadaWeb/Validators/CarroModeloValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoProgramacionAvanzadaWeb.Validators
+{
+    public class CarroModeloValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var modelo = value as string;
+
+            int modeloInt = Int32.Parse(modelo); 
+
+            if (modeloInt > 2026)
+            {
+                return new ValidationResult("Validación personalizada, El modelo no existe");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
